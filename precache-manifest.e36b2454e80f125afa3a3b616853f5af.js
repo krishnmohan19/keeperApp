c1a88d20036030da2c9e26686993750543caed90
krishnmohan <krishnmohan2819@gmail.com>
2020-12-06 11:46:08 +0530

    Updates

---
+++ precache-manifest.e36b2454e80f125afa3a3b616853f5af.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4efbac55a2d7e471ff72bb693272b50c",
+    "url": "/keeperApp/index.html"
+  },
+  {
+    "revision": "11386d19b5c866479f57",
+    "url": "/keeperApp/static/js/2.e31e5401.chunk.js"
+  },
+  {
+    "revision": "dbe6076f1c6cf570a076",
+    "url": "/keeperApp/static/js/main.c5618e88.chunk.js"
+  },
+  {
+    "revision": "3071bcc64712b314a6ee",
+    "url": "/keeperApp/static/js/runtime-main.2d71c934.js"
+  }
+]);
